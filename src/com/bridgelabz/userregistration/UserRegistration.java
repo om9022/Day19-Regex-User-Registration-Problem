86b5130d0e122cd47e2aff1b107cb54ee2e94010
om9022 <omkarchikane607@gmail.com>
2021-07-17 17:52:06 +0530

    Added : Method to check first name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,8 +1,29 @@
 package com.bridgelabz.userregistration;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to User Registration");
+		UserRegistration details = new UserRegistration();
+		String firstName ="Firstname";
+		details.checkFirstName(firstName);
+	}
+
+	/**
+	 * @param method to check first name 
+	 */
+	private void checkFirstName(String firstName) 
+	{
+		boolean IsMatched = Pattern.compile("^[A-Z]{1}[a-z]{3,}$").matcher(firstName).matches();
+		if(IsMatched)
+		{
+			System.out.println("First name is correct");
+		}
+		else 
+		{
+			System.out.println("First name is invalid");
+		}
 	}
 }
